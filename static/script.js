00e3b6fb45e40da0e76b98277013080b2aaa6948
Alex Lunyov <isuntc@gmail.com>
2023-04-05 00:07:50 +0200

    Preloading

--- static/script.js
+++ static/script.js
@@ -1,7 +1,19 @@
 (() => {
   const preview = document.querySelector('.preview');
 
-  preview?.addEventListener('click', () => {
-    preview.classList.toggle('full');
-  })
+  if (preview) {
+    preview.addEventListener('click', () => {
+      preview.classList.toggle('full');
+    });
+
+    const previewImg = preview.querySelector('img');
+    const preloadImg = new Image();
+
+    preloadImg.onload = () => {
+      previewImg.src = preloadImg.src;
+      preview.classList.add('loaded');
+    };
+    preloadImg.src = previewImg.dataset.src;
+  }
+
 })();
