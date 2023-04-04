261aa61b3f6a98887c8193b1087a86c052226f7c
Alex Lunyov <isuntc@gmail.com>
2023-04-05 00:07:39 +0200

    Progressive thumbs

--- resize.sh
+++ resize.sh
@@ -2,5 +2,5 @@ mkdir -p images
 mkdir -p images/thumbs
 mkdir -p images/full
 
-mogrify -path images/thumbs -resize 1000x1000 -quality 90 photos/*.jpg
+mogrify -path images/thumbs -strip -interlace plane -resize 1000x1000 -quality 90 photos/*.jpg
 mogrify -path images/full -resize 3000x3000 -quality 100 photos/*.jpg
