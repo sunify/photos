c27c521842acd2c48269d2364e10eada367c72a6
Alex Lunyov <isuntc@gmail.com>
2023-04-05 02:30:02 +0200

    og

--- resize.sh
+++ resize.sh
@@ -3,5 +3,5 @@ mkdir -p images
 mkdir -p images/thumbs
 mkdir -p images/full
 
-mogrify -path images/thumbs -interlace plane -resize 1000x1000 -quality 90 photos/*.jpg
+mogrify -path images/thumbs -interlace plane -resize 1200x1200 -quality 90 photos/*.jpg
 mogrify -path images/full -resize 3000x3000 -quality 100 photos/*.jpg
