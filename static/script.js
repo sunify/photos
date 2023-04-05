19f1d739b28e40216be7a3df30be52e1c3520fac
sunify <isuntc@gmail.com>
2023-04-05 00:21:45 +0000

    Deploying to gh-pages from @ sunify/photos@2f259ba72cd524ffa72d39510cb045b002712cb4 🚀

--- static/script.js
+++ static/script.js
@@ -2,11 +2,8 @@
   const preview = document.querySelector('.preview');
 
   if (preview) {
-    preview.addEventListener('click', () => {
-      preview.classList.toggle('full');
-    });
-
     const previewImg = preview.querySelector('img');
+    const previewPlaceholder = preview.querySelector('.preview-placeholder');
     const preloadImg = new Image();
 
     preloadImg.onload = () => {
@@ -14,6 +11,29 @@
       preview.classList.add('loaded');
     };
     preloadImg.src = previewImg.dataset.src;
-  }
 
+    function handleResize() {
+      const ogAspectRatio = Number(previewImg.dataset.aspect);
+      const isTaller = ogAspectRatio < previewImg.width / previewImg.height;
+      previewPlaceholder.classList.toggle('-v', isTaller);
+      previewPlaceholder.classList.toggle('-h', !isTaller);
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize, { passive: true });
+
+    preview.addEventListener('click', () => {
+      preview.classList.toggle('full');
+      handleResize();
+    });
+
+    window.addEventListener('keyup', (e) => {
+      if(e.key === 'Escape') {
+        if (preview.classList.contains('full')) {
+          preview.classList.remove('full');
+        } else {
+          window.location = '..';
+        }
+      }
+    })
+  }
 })();
