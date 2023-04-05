72e656e7116dca10f1160bc6e7cd2531ae14e205
Alex Lunyov <isuntc@gmail.com>
2023-04-06 00:50:34 +0200

    Some play with quality

--- resize.sh
+++ resize.sh
@@ -3,5 +3,5 @@ mkdir -p images
 mkdir -p images/thumbs
 mkdir -p images/full
 
-mogrify -path images/thumbs -interlace plane -resize 900x900 -quality 80 photos/*.jpg
-mogrify -path images/full -resize 2000x2000 -quality 90 photos/*.jpg
+mogrify -path images/thumbs -units PixelsPerInch -density 72 -interlace plane -resize 900x900 -quality 80 photos/*.jpg
+mogrify -path images/full -units PixelsPerInch -density 72 -interlace plane -resize 2500x2500 -quality 80 photos/*.jpg
