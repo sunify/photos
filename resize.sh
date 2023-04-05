405df232b4bf0cffbfff09975729b825b339bfbe
Alex Lunyov <isuntc@gmail.com>
2023-04-06 00:23:14 +0200

    Resize images

--- resize.sh
+++ resize.sh
@@ -3,5 +3,5 @@ mkdir -p images
 mkdir -p images/thumbs
 mkdir -p images/full
 
-mogrify -path images/thumbs -interlace plane -resize 1200x1200 -quality 90 photos/*.jpg
-mogrify -path images/full -resize 3000x3000 -quality 100 photos/*.jpg
+mogrify -path images/thumbs -interlace plane -resize 900x900 -quality 80 photos/*.jpg
+mogrify -path images/full -resize 2000x2000 -quality 90 photos/*.jpg
