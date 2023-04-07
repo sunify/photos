4cadc177a83716daaa7181a492cffaf9149d3ce8
Alex Lunyov <isuntc@gmail.com>
2023-04-07 19:42:08 +0200

    Up the quality of hi-res

--- resize.sh
+++ resize.sh
@@ -4,4 +4,4 @@ mkdir -p images/thumbs
 mkdir -p images/full
 
 mogrify -path images/thumbs -units PixelsPerInch -density 72 -interlace plane -resize 900x900 -quality 80 photos/*.jpg
-mogrify -path images/full -units PixelsPerInch -density 72 -interlace plane -resize 2500x2500 -quality 80 photos/*.jpg
+mogrify -path images/full -units PixelsPerInch -density 72 -interlace plane -resize 2500x2500 -quality 90 photos/*.jpg
