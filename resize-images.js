122c11491dc2256f79f364ebe8b56b598b92519e
Alex Lunyov <isuntc@gmail.com>
2023-07-02 13:23:49 +0200

    Move images into public and cleanup resize-images script a bit

--- resize-images.js
+++ resize-images.js
@@ -4,6 +4,13 @@ import { exec } from 'child_process';
 
 const execPromise = promisify(exec);
 
+const commonIMParams = '-units PixelsPerInch -density 72 -interlace plane';
+const ogIMParams = `${commonIMParams} -resize 1960x590 -extent 1200x630 -gravity center -quality 70 -strip`;
+const fullIMParams = `${commonIMParams} -resize 2500x2500 -quality 90`;
+const thumbIMParams = `${commonIMParams} -resize 900x900 -quality 80`;
+
+const BASE_PATH = 'public/images';
+
 async function run() {
   const [allPhotos, resizedPhotos] = await Promise.all([
     glob('photos/*.jpg'),
@@ -17,15 +24,9 @@ async function run() {
   );
 
   const filesStr = newPhotos.join(' ');
-  const ogImagesProcess = execPromise(
-    `mogrify -path images/og -units PixelsPerInch -density 72 -interlace plane -resize 1960x590 -extent 1200x630 -gravity center -quality 70 -strip ${filesStr}`
-  );
-  const fullImagesProcess = execPromise(
-    `mogrify -path images/full -units PixelsPerInch -density 72 -interlace plane -resize 2500x2500 -quality 90 ${filesStr}`
-  );
-  const thumbImagesProcess = execPromise(
-    `mogrify -path images/thumbs -units PixelsPerInch -density 72 -interlace plane -resize 900x900 -quality 80 ${filesStr}`
-  );
+  const ogImagesProcess = execPromise(`mogrify -path ${BASE_PATH}/og ${ogIMParams} ${filesStr}`);
+  const fullImagesProcess = execPromise(`mogrify -path ${BASE_PATH}/full ${fullIMParams} ${filesStr}`);
+  const thumbImagesProcess = execPromise(`mogrify -path ${BASE_PATH}/thumbs ${thumbIMParams} ${filesStr}`);
   await Promise.all([ogImagesProcess, fullImagesProcess, thumbImagesProcess]);
 }
 
