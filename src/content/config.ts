e3b68b7a3281fa04c558451001533bd0bd3aa514
Alex Lunyov <isuntc@gmail.com>
2023-07-02 17:27:51 +0200

    Updated types

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,26 @@
 import { defineCollection, z } from 'astro:content';
 
 const photos = defineCollection({
-	// Type-check frontmatter using a schema
-	type: 'data',
-	schema: z.object({
-		id: z.string(),
-		title: z.string(),
-		color: z.string(),
-		shutterSpeed: z.number(),
-		iso: z.number(),
-		f: z.string(),
-		focalLength: z.number(),
-		camera: z.string(),
-	}),
+  // Type-check frontmatter using a schema
+  type: 'data',
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    color: z.string(),
+    exif: z.object({
+      image: z.object({
+        Make: z.string(),
+        Model: z.string(),
+      }),
+      exif: z.object({
+        ExposureTime: z.number(),
+        FNumber: z.number(),
+        ISO: z.number(),
+        FocalLengthIn35mmFormat: z.number(),
+        LensModel: z.string().optional(),
+      }),
+    }),
+  }),
 });
 
 export const collections = { photos };
