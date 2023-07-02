efc5bdc7897b9faca8397ea5662c0300ab27edfd
Alex Lunyov <isuntc@gmail.com>
2023-07-02 18:32:47 +0200

    Static version of puzzle header

---
+++ src/components/PuzzleHeader.svelte
@@ -0,0 +1,162 @@
+<script>
+  let positions = Array.from({ length: 9 }, (_, i) => i + 1);
+  const letters = 'alexlunёv';
+</script>
+
+<header class="cover">
+  <div class="cover-box">
+    <div class="pieces">
+      {#each positions as pos, i}
+        <div class="piece" data-num="{i + 1}" data-pos="{pos}" />
+      {/each}
+      </div>
+
+      <div class="letters">
+      {#each positions as pos, i}
+        <div class="letter">
+          <span>{letters[i]}</span>
+        </div>
+      {/each}
+      </div>
+  </div>
+</header>
+
+<style>
+.cover {
+  --cell-size: 100px;
+  --gap-size: 20px;
+  --over-size: 25vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(var(--cell-size) * 3 + var(--over-size));
+  position: relative;
+}
+
+.cover-box {
+  position: relative;
+
+  width: calc(var(--cell-size) * 3 + var(--gap-size) * 2);
+  height: calc(var(--cell-size) * 3 + var(--gap-size) * 2);
+  gap: var(--gap-size);
+}
+
+.piece {
+  position: absolute;
+  width: var(--cell-size);
+  height: var(--cell-size);
+  background-image: url(cover.jpg);
+  background-size: calc(var(--cell-size) * 3) calc(var(--cell-size) * 3);
+  transition: top 0.3s ease-in-out, left 0.3s ease-in-out;
+}
+
+.letter {
+  position: absolute;
+  width: var(--cell-size);
+  height: var(--cell-size);
+  pointer-events: none;
+}
+
+.letter span {
+  color: #FFF;
+  font-size: calc(var(--cell-size) / 2.5);
+  line-height: calc(var(--cell-size) / 2.5);
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.piece[data-num="1"] {
+  background-position: 0 0;
+}
+
+.piece[data-num="2"] {
+  background-position: calc(var(--cell-size) * -1) 0;
+}
+
+.piece[data-num="3"] {
+  background-position: calc(var(--cell-size) * -2) 0;
+}
+
+.piece[data-num="4"] {
+  background-position: 0 calc(var(--cell-size) * -1);
+}
+
+.piece[data-num="5"] {
+  background-position: calc(var(--cell-size) * -1) calc(var(--cell-size) * -1);
+}
+
+.piece[data-num="6"] {
+  background-position: calc(var(--cell-size) * -2) calc(var(--cell-size) * -1);
+}
+
+
+.piece[data-num="7"] {
+  background-position: 0 calc(var(--cell-size) * -2);
+}
+
+.piece[data-num="8"] {
+  background-position: calc(var(--cell-size) * -1) calc(var(--cell-size) * -2);
+}
+
+.piece[data-num="9"] {
+  background-position: calc(var(--cell-size) * -2) calc(var(--cell-size) * -2);
+}
+
+.piece[data-pos="1"],
+.letter:nth-child(1) {
+  left: 0;
+  top: 0;
+}
+
+.piece[data-pos="2"],
+.letter:nth-child(2) {
+  left: calc(var(--cell-size) + var(--gap-size));
+  top: 0;
+}
+
+.piece[data-pos="3"],
+.letter:nth-child(3) {
+  left: calc(var(--cell-size) * 2 + var(--gap-size) * 2);
+  top: 0;
+}
+
+.piece[data-pos="4"],
+.letter:nth-child(4) {
+  left: 0;
+  top: calc(var(--cell-size) + var(--gap-size));
+}
+
+.piece[data-pos="5"],
+.letter:nth-child(5) {
+  left: calc(var(--cell-size) + var(--gap-size));
+  top: calc(var(--cell-size) + var(--gap-size));
+}
+
+.piece[data-pos="6"],
+.letter:nth-child(6) {
+  left: calc(var(--cell-size) * 2 + var(--gap-size) * 2);
+  top: calc(var(--cell-size) + var(--gap-size));
+}
+
+
+.piece[data-pos="7"],
+.letter:nth-child(7) {
+  left: 0;
+  top: calc(var(--cell-size) * 2 + var(--gap-size) * 2);
+}
+
+.piece[data-pos="8"],
+.letter:nth-child(8) {
+  left: calc(var(--cell-size) + var(--gap-size));
+  top: calc(var(--cell-size) * 2 + var(--gap-size) * 2);
+}
+
+.piece[data-pos="9"],
+.letter:nth-child(9) {
+  left: calc(var(--cell-size) * 2 + var(--gap-size) * 2);
+  top: calc(var(--cell-size) * 2 + var(--gap-size) * 2);
+}
+</style>
