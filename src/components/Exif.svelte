6547b9a1727d7806ea7c7579fe9f082d671e13b8
Alex Lunyov <isuntc@gmail.com>
2023-07-04 20:00:54 +0200

    Hide lens info

--- src/components/Exif.svelte
+++ src/components/Exif.svelte
@@ -1,35 +1,37 @@
 <script lang="ts">
-export let fNumber: number;
-export let exposureTime: number;
-export let iso: number;
-export let focalLength: number;
-export let lens: string | null = null;
-export let camera: string;
+  export let fNumber: number;
+  export let exposureTime: number;
+  export let iso: number;
+  export let focalLength: number;
+  export let lens: string | null = null;
+  export let camera: string;
 
-function formatCamera(camera: string, lens: string | null) {
-  const replacements: Record<string, string> = {
-    'RICOH GR III': 'GRIII'
-  };
+  function formatCamera(camera: string, lens?: string | null) {
+    const replacements: Record<string, string> = {
+      'RICOH GR III': 'GRIII',
+    };
 
-  let result = replacements[camera] || camera;
-  if (lens) {
-    result += ` (${lens})`;
+    let result = replacements[camera] || camera;
+    if (lens) {
+      result += ` (${lens})`;
+    }
+
+    return result;
   }
 
-  return result;
-}
-
-function formatExposure(exposureTime: number): string {
-  if (exposureTime < 1) {
-    return `1/${1/exposureTime}`;
-  } else {
-    return String(exposureTime);
+  function formatExposure(exposureTime: number): string {
+    if (exposureTime < 1) {
+      return `1/${1 / exposureTime}`;
+    } else {
+      return String(exposureTime);
+    }
   }
-}
 </script>
 
 <div class="exif">
-  <div class="model">{formatCamera(camera, lens)}</div>
+  <div class="model" title={formatCamera(camera, lens)}>
+    {formatCamera(camera)}
+  </div>
   <div class="focal-length">{focalLength}</div>
   <div class="exposure">{formatExposure(exposureTime)}</div>
   <div class="fstop">{fNumber}</div>
@@ -37,28 +39,28 @@ function formatExposure(exposureTime: number): string {
 </div>
 
 <style>
-.exif {
-  font-size: 14px;
-  display: flex;
-  gap: 15px;
-  justify-content: center;
-}
+  .exif {
+    font-size: 14px;
+    display: flex;
+    gap: 15px;
+    justify-content: center;
+  }
 
-.exposure::after {
-  content: 's';
-}
+  .exposure::after {
+    content: 's';
+  }
 
-.fstop::before {
-  content: 'f';
-  font-style: italic;
-  margin-right: 2px;
-}
+  .fstop::before {
+    content: 'f';
+    font-style: italic;
+    margin-right: 2px;
+  }
 
-.iso::before {
-  content: 'ISO ';
-}
+  .iso::before {
+    content: 'ISO ';
+  }
 
-.focal-length::after {
-  content: 'mm';
-}
+  .focal-length::after {
+    content: 'mm';
+  }
 </style>
