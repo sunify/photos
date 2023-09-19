e42e773ac046acc8d70078992ce89da447a34d55
Alex Lunyov <isuntc@gmail.com>
2023-09-19 21:51:09 +0200

    Increase resolution for terminal preview

---
+++ scripts/print-all.js
@@ -0,0 +1,23 @@
+import { glob } from 'glob';
+import { printImage } from './common/print-image.js';
+
+async function getAllPhotos() {
+  return glob('source-photos/*.jpg');
+}
+
+async function main() {
+  await printImage('source-photos/P1130273.jpg');
+  // await printImage('source-photos/P1100757.jpg');
+  // await printImage('source-photos/R0011413.jpg');
+  await printImage('source-photos/P1110716.jpg');
+  await printImage('source-photos/R0010094.jpg');
+  await printImage('source-photos/R0010347.jpg');
+  // await getAllPhotos().then(async (photos) => {
+  //   for (const photo of photos) {
+  //     console.log(photo);
+  //     await printImage(photo);
+  //   }
+  // });
+}
+
+main();
