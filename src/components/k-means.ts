3a338547d4b85b6b6cbe3548e144c89d42fd68cc
Alex Lunyov <isuntc@gmail.com>
2023-09-21 10:42:09 +0200

    Posterize console pic

---
+++ src/components/k-means.ts
@@ -0,0 +1,81 @@
+type DistanceFn<T> = (p1: T, p2: T) => number;
+
+type Options<T> = {
+  distanceFn: DistanceFn<T>;
+  centroidFn: (points: T[]) => T;
+};
+
+type Cluster<T> = {
+  center: T,
+  points: T[]
+};
+
+function random(start: number, end: number) {
+  return Math.round(Math.random() * (end - start)) + start;
+}
+
+export function closestPointIndex<T>(points: T[], point: T, distanceFn: DistanceFn<T>) {
+  let result = 0;
+  let pDistance = distanceFn(points[0], point);
+  for (let i = 1; i < points.length; i += 1) {
+    const dst = distanceFn(points[i], point);
+    if (dst < pDistance) {
+      pDistance = dst;
+      result = i;
+    }
+  }
+
+  return result;
+};
+
+export function kMeans<T>(
+  points: T[],
+  k: number,
+  { distanceFn, centroidFn }: Options<T>
+) {
+  const clusters: Array<Cluster<T>> = [];
+  const indicies = new Set<number>();
+  while (indicies.size < k) {
+    indicies.add(random(0, points.length));
+  }
+
+  indicies.forEach((i) => {
+    clusters.push({
+      center: points[i],
+      points: [],
+    });
+  });
+
+  const centerPoint = (points: T[]) => {
+    const centerIndex = closestPointIndex(
+      points,
+      centroidFn(points),
+      distanceFn
+    );
+    return points[centerIndex];
+  };
+
+  const assingPoints = () => {
+    const centers = clusters.map((c) => c.center);
+    points.forEach((point) => {
+      const clusterIndex = closestPointIndex(centers, point, distanceFn);
+      clusters[clusterIndex].points.push(point);
+    });
+  };
+
+  const updateCenters = () => {
+    clusters.forEach((cluster) => {
+      cluster.center = centerPoint(cluster.points);
+      cluster.points = [];
+    });
+  };
+
+  assingPoints();
+
+  for (let i = 0; i < 25; i += 1) {
+    updateCenters();
+    assingPoints();
+  }
+
+  return clusters.map((c) => c.points);
+}
