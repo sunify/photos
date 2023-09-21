790f4a1c36dfb2cd5cb71cfd362a7a079d112609
Alex Lunyov <isuntc@gmail.com>
2023-09-21 02:15:32 +0200

    Better prints in stdout

--- scripts/print-all.js
+++ scripts/print-all.js
@@ -6,12 +6,12 @@ async function getAllPhotos() {
 }
 
 async function main() {
-  await printImage('source-photos/P1130273.jpg');
+  // await printImage('source-photos/R0010094.jpg');
   // await printImage('source-photos/P1100757.jpg');
-  // await printImage('source-photos/R0011413.jpg');
-  await printImage('source-photos/P1110716.jpg');
-  await printImage('source-photos/R0010094.jpg');
-  await printImage('source-photos/R0010347.jpg');
+  await printImage('source-photos/R0011413.jpg');
+  // await printImage('source-photos/P1110716.jpg');
+  // await printImage('source-photos/R0008031.jpg');
+  // await printImage('source-photos/R0009510.jpg');
   // await getAllPhotos().then(async (photos) => {
   //   for (const photo of photos) {
   //     console.log(photo);
